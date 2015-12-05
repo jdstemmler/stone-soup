b7201c7cbeb122c4a8f74eba23c68a7009d1bb9e
Jayson Stemmler <jdstemmler@gmail.com>
2015-12-05 13:17:29 -0800

    added lots of comments and updated main script

--- data-retrieval/get_nytimes_metadata.py
+++ data-retrieval/get_nytimes_metadata.py
@@ -13,7 +13,7 @@ from gtools.settings import load_setting
 
 
 def format_api_url(api_key):
-    params = {}
+    """Format the api call for getting recipes given an api key"""
     url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
     fields = ['_id', 'headline', 'byline', 'document_type', 'web_url', 'pub_date', 'lead_paragraph', 'snippet']
     params = {'fq': 'type_of_material:("Recipe")',
@@ -25,6 +25,7 @@ def format_api_url(api_key):
 
 
 def query_api(url, params):
+    """send the URL and the parameters to the api, return the response"""
     response = requests.get(url, params=params)
     if response.status_code != 200:
         print('WARNING', response.status_code)
@@ -34,6 +35,7 @@ def query_api(url, params):
 
 
 def get_number_of_results(response):
+    """get the number of results returned by the api query"""
     json = getattr(response, 'json')
     if json() is None:
         return None
@@ -42,6 +44,7 @@ def get_number_of_results(response):
 
 
 def parse_api_response(response):
+    """parse the api query to get out the documents"""
     json = getattr(response, 'json')
     if json is None:
         return None
@@ -51,7 +54,17 @@ def parse_api_response(response):
     return docs
 
 
+def insert_into_mongo(table, rows):
+    """insert a list of rows into mongo, checking that the rows don't already exist"""
+    for row in rows:
+        try:
+            table.insert_one(row)
+        except DuplicateKeyError:
+            continue
+
+
 def get_single_query(key, tab, page=0):
+    """send the results of a single query (one page) into the mongoDB"""
     url, params = format_api_url(key)
     params['page'] = page
 
@@ -61,104 +74,135 @@ def get_single_query(key, tab, page=0):
     return docs
 
 
-def paginate(key, tab, sleep=5, **kwargs):
-    prior_tab_len = tab.find().count()
+def paginate(key, tab, sleep=5, verbose=True, **kwargs):
+    """paginate through the api results while there are still documents"""
+
+    prior_tab_len = tab.find().count()  # number of current docs prior to pagination
+
+    # get the first page of results
     url, params = format_api_url(key)
     params.update(kwargs)
     response = query_api(url, params)
-    #print(response.url)
+
     while response.status_code == 200:
-        print("Getting Page {}".format(params['page']))
+        # if a valid response, check for documents
+        if verbose: print("Getting Page {}".format(params['page']))
         docs = parse_api_response(response)
         if docs is None:
+            # break out if no documents
             break
+
+        # if we have documents, insert them into mongo
         insert_into_mongo(tab, docs)
-        params['page'] += 1
-        time.sleep(sleep)
+
+        params['page'] += 1  # increment page number
+        time.sleep(sleep)    # take a small break before next query
+
+        # get the next response
         response = query_api(url, params)
-    final_tab_len = tab.find().count()
-    print("Inserted {} new metadata rows".format(final_tab_len - prior_tab_len))
-    print("Current Table Size: {} entries\n".format(final_tab_len))
+
+    final_tab_len = tab.find().count()  # number of docs after pagination
+
+    if verbose:
+        print("Inserted {} new metadata rows".format(final_tab_len - prior_tab_len))
+        print("Current Table Size: {} entries\n".format(final_tab_len))
 
 
-def paginate_by_date(key, tab, start_date='19000101', init_window=0):
+def paginate_by_date(key, tab, start_date='19000101', init_window=0, verbose=True):
+    """does most of the heaving lifting, paging through a date range
+    Inputs:
+        :param key: the api key for reaching NYTimes
+        :param tab: the table to insert the documents into
+        :param start_date: the date to start searching
+        :param init_window: size of window (in days) to start searching
+        :param verbose:
+
+    """
+    # initialize date settings
     window = datetime.timedelta(days=init_window)
     dfmt = '%Y%m%d'
     start = datetime.datetime.strptime(start_date, dfmt)
     end = start + window
-    print("Starting with {}".format(start))
-    print("End Date: {}".format(end))
-    print("Window Size of {} Days\n".format(window.days))
+    if verbose:  # print some information
+        print("Starting with {}".format(start))
+        print("End Date: {}".format(end))
+        print("Window Size of {} Days\n".format(window.days))
+
+    # don't query for date ranges after the current date and time
     while start <= datetime.datetime.today():
-        # test the window. can get max 1000 hits
+
+        # send an ititial query to the api and get a response
         url, params = format_api_url(key)
         params.update(begin_date=start.strftime(dfmt),
                       end_date=end.strftime(dfmt))
         response = query_api(url, params)
 
+        # test to see how many total results. can only paginate up to 1000 results.
         while get_number_of_results(response) > 1000:
+            # if more than 1000 results, cut the window in two
             window = datetime.timedelta(days=(window / 2).days)
-            print("Reducing Window to {} Days".format(window.days))
+            if verbose: print("Reducing Window to {} Days".format(window.days))
+
+            # set the new end date
             end = start + window
-            print("New End Date: {}\n".format(end))
+            if verbose: print("New End Date: {}\n".format(end))
+
+            # perform the query with the new dates and check number of results
             url, params = format_api_url(key)
             params.update(begin_date=start.strftime(dfmt),
                           end_date=end.strftime(dfmt))
             response = query_api(url, params)
 
-        paginate(key, tab,
+        # we can now paginate and get all the results
+        paginate(key, tab, verbose=verbose,
                  begin_date=start.strftime(dfmt),
-                 end_date=end.strftime(dfmt))
+                 end_date=end.strftime(dfmt)
+                 )
+
+        # set the new start and end dates
         start += window
         end = start + window
-        print("New Start Date: {}".format(start))
-        print("New End Date: {}".format(end))
-        print("Window: {} Days\n".format(window.days))
-        time.sleep(10)
+
+        if verbose:
+            print("New Start Date: {}".format(start))
+            print("New End Date: {}".format(end))
+            print("Window: {} Days\n".format(window.days))
+
+        time.sleep(10)  # take a short nap
+
     print("Complete")
-
-    # for i in range(20):
-    #     startd = endd - datetime.timedelta(days=window)
-    #
-    #     st = startd.strftime('%Y%m%d')
-    #     en = endd.strftime('%Y%m%d')
-    #     print("{} to {}".format(st, en))
-    #
-    #     paginate(key, tab, begin_date=st, end_date=en)
-    #
-    #     endd -= datetime.timedelta(days=window+1)
-    #
-    #     time.sleep(30)
-
-
-def insert_into_mongo(table, rows):
-    for row in rows:
-        try:
-            table.insert_one(row)
-        except DuplicateKeyError:
-            continue
 
 
 def main(start_date):
+    """called by the script to do the processing"""
 
+    # set the location of the settings file
     settings_file = os.path.join(os.getenv("CAPSTONE_DIR"), 'settings.json')
 
-    database = load_setting(settings_file, 'db_name')
-    nyt_api_key = load_setting(settings_file, 'NYT_API_KEY')
+    # set some settings
+    database = load_setting(settings_file, 'db_name')  # name of mongodb database
+    nyt_api_key = load_setting(settings_file, 'NYT_API_KEY')  #NYTimes API Key
 
+    # open the connection to the database, get the table
     client = MongoClient()
     db = client[database]
     table = db['recipe-metadata']
 
+    # paginate through the search results
+    # starting at the start date specified at runtime and a 2000 day initial window
     paginate_by_date(nyt_api_key, table, start_date=start_date, init_window=2000)
 
+    # close the connection to the client
     client.close()
 
 
 if __name__ == "__main__":
+    # set the start date to be today
     start_date = datetime.date.today().strftime('%Y%m%d')
 
+    # check for additional inputs. If present, set the new start date
     if len(sys.argv) > 1:
         start_date = sys.argv[-1]
 
+    # run main, passing in the start date
     main(start_date=start_date)
--- data-retrieval/main.sh
+++ data-retrieval/main.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Executing NYTimesCooking Data Retreival"
-./nytimes.py
+echo "Executing NYTimesCooking Metadata Retrieval"
+python ${CAPSTONE_DIR}/data-retrieval/get_nytimes_metadata.py 19000101
+
+echo "Extracting NYTimes Metadata to File"
+python ${CAPSTONE_DIR}/data-retrieval/extract_nytimes_urls.py
+
+echo "Executing NYTimesCooking Document Retrieval"
+python ${CAPSTONE_DIR}/data-retrieval/get_nytimes_recipes.py
