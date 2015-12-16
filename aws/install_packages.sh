d0240ea21d55c15ac0e40a43a48febd91cb3d9f2
Jayson Stemmler <jdstemmler@gmail.com>
2015-12-16 22:36:40 +0000

    make exec, add some comments

--- aws/install_packages.sh
+++ aws/install_packages.sh
@@ -2,5 +2,6 @@
 
 sudo apt-update && sudo apt-get -y dist-upgrade
 
+# https://github.com/yyuu/pyenv/wiki/Common-build-problems
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
 sudo apt-get install git
