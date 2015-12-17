6c9b778069a56a0439109bd4b27de15bf0494a10
Jayson Stemmler <jdstemmler@gmail.com>
2015-12-17 09:01:46 -0800

    added pymongo

--- aws/pip_packages.sh
+++ aws/pip_packages.sh
@@ -8,5 +8,6 @@ pip install --upgrade scikit-learn
 pip install --upgrade beautifulsoup4
 pip install --upgrade flask
 pip install --upgrade requests
+pip install --upgrade pymongo
 
 pip install -e $CAPSTONE_DIR/tools
