d2d352a99fbb6e3723840ca8d1d6f812a913148b
Jayson Stemmler <jdstemmler@gmail.com>
2016-01-07 12:23:32 -0800

    remove recipe parsing

--- data/data_processing.sh
+++ data/data_processing.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd $CAPSTONE_DIR/data/parsing
-python parse_nyt_recipes.py
+# cd $CAPSTONE_DIR/data/parsing
+# python parse_nyt_recipes.py
 
+# make pickles out of the bag of ingredients and topic modeling
 cd $CAPSTONE_DIR/data/modeling
 python make_bag_of_ingredients.py
 python topic_modeling.py
