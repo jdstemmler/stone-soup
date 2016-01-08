aa0045ff7a0d365b5128e4998fed672c5d5474e8
Jayson Stemmler <jdstemmler@gmail.com>
2016-01-08 13:04:32 -0800

    reduce number of direction cols in tfidf

--- data/data_processing.sh
+++ data/data_processing.sh
@@ -5,5 +5,10 @@
 
 # make pickles out of the bag of ingredients and topic modeling
 cd $CAPSTONE_DIR/data/modeling
+echo "Processing Bag of Ingredients"
 python make_bag_of_ingredients.py
+
+echo "Performing Matrix Factorization"
 python topic_modeling.py
+
+echo "Complete"
