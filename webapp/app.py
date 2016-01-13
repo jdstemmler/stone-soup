7fd30485e53ddf077d051d3c695e00cd4df4571b
Jayson Stemmler <jdstemmler@gmail.com>
2016-01-12 21:15:20 -0800

    added faq page and auth for dashboard

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, render_template, send_from_directory
-
+from flask import Flask, request, render_template, send_from_directory, Response
+from functools import wraps
 import sys
 import os
 import pickle
@@ -71,7 +71,7 @@ def index():
 
 @app.route('/advanced')
 def advanced():
-    return "This little light of mine..."
+    return "This feature is not implemented ..."
 
 
 @app.route('/about')
@@ -95,7 +95,38 @@ def contact():
     return render_template('contact.html', google_site_key=google_site_key)
 
 
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'bluebird'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @app.route('/dashboard')
+@requires_auth
 def dashboard():
     responses = tab.find({}, {'_id': 0, 'timestamp': 1, 'feedback': 1})
     feedback = []
